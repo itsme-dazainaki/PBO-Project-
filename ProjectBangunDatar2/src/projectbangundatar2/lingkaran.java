96c618745a45254e818cb69fbe0fc518aa517934
itsme-dazainaki <72004911+itsme-dazainaki@users.noreply.github.com>
2021-10-11 22:52:46 +0700

    Add files via upload

---
+++ ProjectBangunDatar2/src/projectbangundatar2/lingkaran.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author Radaza Putri
+ */
+public class lingkaran {
+    //rumus luas lingkaran 
+    int luasL(int y){
+        int luas = y*y;
+        return luas;
+    }
+    double luasL(double y){
+        double luas = y*y;
+        return luas;
+    }
+    
+    //rumus keliling lingkaran
+    int kelilingL(int y){
+        int kel = 2*y;
+        return kel;
+    }
+    double kelilingL(double y){
+        double kel = 2*y;
+        return kel;
+    }
+    
+}
